e1c2a8312b436624ad05c8ce285bc36d05683bc7
Ali CANLI <alicanli1995@gmail.com>
2022-07-17 12:33:35 +0300

    Outbox Message and Scheduler class bug fixed.

--- order-service/order-messaging/src/main/java/com/food/order/system/messaging/listener/kafka/PaymentResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/food/order/system/messaging/listener/kafka/PaymentResponseKafkaListener.java
@@ -1,5 +1,6 @@
 package com.food.order.system.messaging.listener.kafka;
 
+import com.food.order.system.domain.exception.OrderNotFoundException;
 import com.food.order.system.kafka.consumer.KafkaConsumer;
 import com.food.order.system.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.food.order.system.kafka.order.avro.model.PaymentStatus;
@@ -7,6 +8,7 @@ import com.food.order.system.messaging.mapper.OrderMessagingDataMapper;
 import com.food.order.system.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -33,20 +35,31 @@ public class PaymentResponseKafkaListener implements KafkaConsumer<PaymentRespon
                         @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
                         @Header(KafkaHeaders.OFFSET) List<Long> offSets) {
 
-        log.info("{} number of payment responses received with keys : {} , partitions : {} , offsets : {}",
-                messages.size(), keys, partitions, offSets);
+        log.info("{} number of payment responses received with keys:{}, partitions:{} and offsets: {}",
+                messages.size(),
+                keys.toString(),
+                partitions.toString(),
+                offSets.toString());
 
-        messages.forEach(message -> {
-            if (PaymentStatus.COMPLETED.equals(message.getPaymentStatus())) {
-                log.info("Processing successful payment response for order id: {}", message.getOrderId());
-                paymentResponseMessageListener.paymentCompleted(orderMessagingDataMapper.
-                        paymentResponseAvroModelToPaymentResponse(message));
-            }
-            else if (PaymentStatus.FAILED.equals(message.getPaymentStatus()) ||
-                     PaymentStatus.CANCELLED.equals(message.getPaymentStatus())) {
-                log.info("Processing failed payment response for order id: {}", message.getOrderId());
-                paymentResponseMessageListener.paymentCancelled(orderMessagingDataMapper.
-                        paymentResponseAvroModelToPaymentResponse(message));
+        messages.forEach(paymentResponseAvroModel -> {
+            try {
+                if (PaymentStatus.COMPLETED == paymentResponseAvroModel.getPaymentStatus()) {
+                    log.info("Processing successful payment for order id: {}", paymentResponseAvroModel.getOrderId());
+                    paymentResponseMessageListener.paymentCompleted(orderMessagingDataMapper
+                            .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
+                } else if (PaymentStatus.CANCELLED == paymentResponseAvroModel.getPaymentStatus() ||
+                        PaymentStatus.FAILED == paymentResponseAvroModel.getPaymentStatus()) {
+                    log.info("Processing unsuccessful payment for order id: {}", paymentResponseAvroModel.getOrderId());
+                    paymentResponseMessageListener.paymentCancelled(orderMessagingDataMapper
+                            .paymentResponseAvroModelToPaymentResponse(paymentResponseAvroModel));
+                }
+            } catch (OptimisticLockingFailureException e) {
+                //NO-OP for optimistic lock. This means another thread finished the work, do not throw error to prevent reading the data from kafka again!
+                log.error("Caught optimistic locking exception in PaymentResponseKafkaListener for order id: {}",
+                        paymentResponseAvroModel.getOrderId());
+            } catch (OrderNotFoundException e) {
+                //NO-OP for OrderNotFoundException
+                log.error("No order found for order id: {}", paymentResponseAvroModel.getOrderId());
             }
         });
 
